46e6b1b9dbe9200e3ae48822920568e5584e6f3c
beaster431 <1250847805@qq.com>
2026-01-12 21:31:03 +0800

    配置数据库连接和mybatis依赖

---
+++ src/main/java/com/example/rebookbackend/ReBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.rebookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rebookbackend/ReBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rebookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
